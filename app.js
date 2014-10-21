60000287506f12c98bfe682016dd17ce9532596f
nonogon <leslie.strauss@gmail.com>
2014-10-21 15:25:19 -0700

    Last cookie request time logged.

--- app.js
+++ app.js
@@ -2,8 +2,17 @@ var express = require('express');
 var app = express();
 var path = requre('path');
 
+app.use(require('cookie-parser')());
+
 app.get('/', function(req, res){
-  console.log('cookies: ',);
+  var lastRequestTime = req.cookies['last-request-time'];
+  var id = req.cookies.id;
+  if (!id) {
+    id = lastID++;
+  }
+
+  res.cookie('last-request-time', Date.now());
+  res.cookie('id', id);
 
   res.sendFile(path.join(__dirname, 'index.html'));
 });
@@ -11,3 +20,4 @@ app.get('/', function(req, res){
 var server = app.listen(process.env.PORT || 9000, function() {
   console.log('Express server started on %d, server.address().port');
 });
+
